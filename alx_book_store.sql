c9cae3afa59b612828cd77ff22414ed6c9b52b65
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-08-12 18:32:19 +0200

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+USE alx_book_store;
+
+CREATE TABLE Books(
+book_id INT Primary Key,
+title VARCHAR(130),
+author_id (Foreign Key referencing Authors table),
+price DOUBLE,
+publication_date DATE,
+);
+
+CREATE TABLE Authors(
+author_id INT Primary Key,
+author_name VARCHAR(215)    
+);
+
+CREATE TABLE Customers(
+customer_id INT Primary Key,
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders(
+order_id INT Primary Key,
+customer_id (Foreign Key referencing Customers table),
+order_date DATE
+);
+
+CREATE TABLE Order_Details(
+orderdetailid INT Primary Key,
+order_id (Foreign Key referencing Orders table),
+book_id (Foreign Key referencing Books table),
+quantity DOUBLE
+);
