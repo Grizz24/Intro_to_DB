ea0e7642313da327da07b4b3d57efa00ae754a79
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-08-12 18:38:52 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,7 +22,7 @@ address TEXT
 
 CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
-customer_id (Foreign Key referencing Customers table),
+customer_id (FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)),
 order_date DATE
 );
 
