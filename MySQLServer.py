d02a6aae272102a31846ae9552d0d2529b2da094
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-08-13 16:39:57 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,8 +16,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error: Could not connect or create database. Details: {e}")
+    except mysql.connector.Error as err:
+        print(f"Error: Could not connect or create database. Details: {err}")
 
     finally:
         if connection and connection.is_connected():
